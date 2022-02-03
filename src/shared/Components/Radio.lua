5f64fa7446a241840f99d3d27b756c6535345dc1
Keith <keithk4865@lz95.net>
2022-02-03 12:02:14 -0600

    development

--- src/shared/Components/Radio.lua
+++ src/shared/Components/Radio.lua
@@ -9,19 +9,23 @@ local Radio = Component.new({
 
 function Radio:Construct()
 	self.Spring = Spring.new(0)
-	Spring.Damping = 0.6
-	Spring.Speed = 32
+	self.Spring.Damper = 0.6
+	self.Spring.Speed = 32
 end
 
 function Radio:Start()
 	self.Sound = self.Instance:FindFirstChildWhichIsA("Sound")
-	self.Mesh = self.Instance:FindFirstChildWhichIsA("Mesh")
+	self.Mesh = self.Instance:FindFirstChildWhichIsA("SpecialMesh")
 	self.Size = self.Mesh.Scale
 end
 
 function Radio:HeartbeatUpdate()
-	self.Spring.Target = self.Sound.PlaybackLoudness / (800+(100/3))
-	self.Mesh.Scale = self.Size * self.Spring.Postion
+	if not self.Sound then
+		self.Sound = self.Instance:FindFirstChildWhichIsA("Sound")
+	else
+		self.Spring.Target = self.Sound.PlaybackLoudness / 1000
+		self.Mesh.Scale = self.Size + Vector3.new(0.2, 0.2, 0.2) * self.Spring.Position
+	end
 end
 
 return Radio
